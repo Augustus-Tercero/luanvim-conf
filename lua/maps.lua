a0eb85fd23c18b0ea4bef8bc2c362653c076d649
AGUSTIN MOTA <2030406@upv.edu.mx>
2022-11-06 14:01:34 -0600

    necesito meter programa de teclas

--- lua/maps.lua
+++ lua/maps.lua
@@ -1,7 +1,10 @@
-vim.g.mapleader = ' '
+local api = vim.api
 local function map(m, k, v) -- map(mode, keybind, value)
 	vim.keymap.set(m, k, v, {silent = true})
 end
+
+vim.g.mapleader = ' '
+vim.opt.clipboard:append {'unnamedplus'}
 
 -- Telescope
 map('n', '<leader>fr', '<CMD>Telescope oldfiles<CR>')
@@ -22,3 +25,22 @@ map('n', '<leader>h', '<C-w>h<CR>')
 map('n', '<leader>l', '<C-w>l<CR>')
 map('n', '<leader>j', '<C-w>j<CR>')
 map('n', '<leader>k', '<C-w>k<CR>')
+
+-- NERDTree
+map('n', '<leader>n', ':NERDTreeToggle<CR>')
+
+-- Latex
+api.nvim_create_autocmd(
+	"FileType", { pattern = { "tex", "latex" }, command = [[map  <C-B>      bdwi\begin{<ESC>pa}<CR>\end{<ESC>pa}<ESC>O]] }
+)
+
+-- show cursor line only in active window
+local cursorGrp = api.nvim_create_augroup("CursorLine", { clear = true })
+api.nvim_create_autocmd(
+  { "InsertLeave", "WinEnter" },
+  { pattern = "*", command = "set cursorline", group = cursorGrp }
+)
+api.nvim_create_autocmd(
+  { "InsertEnter", "WinLeave" },
+  { pattern = "*", command = "set nocursorline", group = cursorGrp }
+)
